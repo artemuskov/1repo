c23462482d3d3774dfb9ba5c2ad106a5e7a09cff
Artem Uskov <artem.uskov@bazaarvoice.com>
2018-12-19 18:42:46 +0200

    add deploy content

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws cloudformation create-stack --region us-east-1 --stack-name artem-deploy --template-body file://jenkins.yaml
+aws cloudformation create-stack --region us-east-1 --stack-name artem-deploy --template-body file://jenkins.yaml --capabilities CAPABILITY_IAM
